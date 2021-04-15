7ea20e269a84e67c9f234e62f2d0e70d6a490df2
WeForgot <jnew2314@gmail.com>
2021-04-15 17:06:50 -0400

    Cleaup

--- model/model.py
+++ model/model.py
@@ -26,36 +26,7 @@ from model.datasets import SADataset
 from model.utils import get_parameter_count, Vocabulary
 
 load_dotenv()
-'''
-class GEGLU(nn.Module):
-    def __init__(self, dim_in, dim_out):
-        super().__init__()
-        self.proj = nn.Linear(dim_in, dim_out * 2)
-    
-    def forward(self, x):
-        x, gate = self.proj(x).chunk(2, dim = -1)
-        return x * F.gelu(gate)
 
-class FeedForward(nn.Module):
-    def __init__(self, dim, dim_out = None, mult = 4, glu = False, dropout = 0.0):
-        super().__init__()
-        inner_dim = int(dim * mult)
-        dim_out = dim_out if dim_out is not None else dim
-        project_in = nn.Sequential(
-            nn.Linear(dim, inner_dim),
-            nn.GELU()
-        ) if not glu else GEGLU(dim, inner_dim)
-
-        self.net = nn.Sequential(
-            project_in,
-            nn.LayerNorm(inner_dim),
-            nn.Dropout(p=dropout),
-            nn.Linear(inner_dim, dim_out)
-        )
-    
-    def forward(self, x):
-        return self.net(x)
-'''
 class BasicEncoder(nn.Module):
     def __init__(self, patch_size = 32, dim = 16, e_depth = 6, e_heads = 8, mlp_dim = 128, encoder_type = 'vit'):
         super(BasicEncoder, self).__init__()
