3e3b95f8ab17d363ef5f33463c9f344d8fb270a9
WeForgot <jnew2314@gmail.com>
2021-04-25 11:53:07 -0400

    Moved the instance norm to in input latent rather than after the summing

--- model/style_model.py
+++ model/style_model.py
@@ -14,44 +14,6 @@ class DepthWiseConv2d(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-
-class Conv2DMod(nn.Module):
-    def __init__(self, in_chan, out_chan, kernel, demod=True, stride=1, dilation=1, eps = 1e-8, **kwargs):
-        super().__init__()
-        self.filters = out_chan
-        self.demod = demod
-        self.kernel = kernel
-        self.stride = stride
-        self.dilation = dilation
-        self.weight = nn.Parameter(torch.randn((out_chan, in_chan, kernel, kernel)))
-        self.eps = eps
-        nn.init.kaiming_normal_(self.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
-
-    def _get_same_padding(self, size, kernel, dilation, stride):
-        return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
-
-    def forward(self, x, y):
-        b, c, h, w = x.shape
-
-        w1 = y[:, None, :, None, None]
-        w2 = self.weight[None, :, :, :, :]
-        weights = w2 * (w1 + 1)
-
-        if self.demod:
-            d = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
-            weights = weights * d
-
-        x = x.reshape(1, -1, h, w)
-
-        _, _, *ws = weights.shape
-        weights = weights.reshape(b * self.filters, *ws)
-
-        padding = self._get_same_padding(h, self.kernel, self.dilation, self.stride)
-        x = F.conv2d(x, weights, padding=padding, groups=b)
-
-        x = x.reshape(-1, self.filters, h, w)
-        return x
-
 class StyleFormer(nn.Module):
     def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.0):
         super().__init__()
@@ -66,8 +28,7 @@ class StyleFormer(nn.Module):
 
     def forward(self, x, styles):
         for idx, (ln, attn, ff) in enumerate(self.layers):
-            x = x + styles[idx]
-            x = ln(x)
+            x = x + ln(styles[idx])
             x = attn(x)
             x = ff(x) + x
         return x
