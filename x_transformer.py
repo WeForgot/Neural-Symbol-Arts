Added a cool scheduler from Transformers, fixed params to be closer to FB's works, removed gradient clipping for now

x_transformer.py
@@ -17,14 +17,14 @@ from tqdm import tqdm
 #from byol_pytorch import BYOL
 #from model.mobilenetv3 import mobilenet_v3_small
 from vit_pytorch import Dino
-from model.custom_gmlp import gMLPVision
+from model.custom_vit import ViT
 #from x_transformers import ContinuousTransformerWrapper, Decoder
 from linear_attention_transformer import LinearAttentionTransformer
 from linear_attention_transformer.autopadder import Autopadder
 
 
 from model.datasets import SADataset
-from model.utils import Vocabulary, convert_numpy_to_saml, get_parameter_count, load_data, load_image
+from model.utils import Vocabulary, convert_numpy_to_saml, get_parameter_count, load_data, load_image, get_cosine_with_hard_restarts_schedule_with_warmup
 
 def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
     """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
@@ -86,6 +86,7 @@ class XEncoder(nn.Module):
     def __init__(self, image_size, patch_size, dim, depth, heads):
         super(XEncoder, self).__init__()
         #self.encoder = mobilenet_v3_small(dim)
+        '''
         self.encoder = gMLPVision(
             image_size = image_size,
             patch_size = patch_size,
@@ -94,6 +95,8 @@ class XEncoder(nn.Module):
             channels = 3,
             prob_survival = 0.9
         )
+        '''
+        self.encoder = ViT(image_size = image_size, patch_size = patch_size, dim = dim, depth = depth, heads = heads, mlp_dim = 1)
         self.to_latent = nn.Identity()
     
     def forward(self, x):
@@ -238,7 +241,7 @@ def piecewise_decay(epoch, steps, alphas):
 def linear_decay(epoch, start, end):
     return start + min(end, ((end-start)/float(epoch)))
 
-def pretrain_encoder(model: nn.Module, image_size, train_data, valid_data, device, batch_size = 32, max_epochs = 500, max_patience = 5, batch_metrics=True):
+def pretrain_encoder(model: nn.Module, image_size, train_data, valid_data, device, batch_size = 32, max_epochs = 100, max_patience = 5, batch_metrics=True):
     learner = Dino(
         model,
         image_size = image_size,
@@ -246,13 +249,15 @@ def pretrain_encoder(model: nn.Module, image_size, train_data, valid_data, devic
     ).to(device)
 
 
-    opt = AdaBelief(learner.parameters(), lr=1e-2, weight_decay=1e-5, print_change_log=False)
+    #opt = AdaBelief(learner.parameters(), lr=1e-2, weight_decay=1e-5, print_change_log=False)
     #scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0=125)
-    #opt = optim.SGD(learner.parameters(), lr=1e-2, momentum=0.9)
-    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0 = 125)
+    
 
     train_dataloder = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True)
     valid_dataloader = DataLoader(valid_data, batch_size=batch_size, shuffle=True, drop_last=True)
+
+    opt = optim.AdamW(learner.parameters(), lr=1e-2, weight_decay=0.04)
+    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0 = len(train_dataloder) * 10, eta_min = 1e-6)
     
     best_loss = None
     best_model = None
@@ -310,7 +315,7 @@ def pretrain_encoder(model: nn.Module, image_size, train_data, valid_data, devic
 
 # The main function
 def main():
-    x_settings = {'image_size': 192, 'patch_size': 16, 'dim': 64, 'e_depth': 2, 'e_heads': 6, 'emb_dim':4, 'd_depth': 2, 'd_heads': 8, 'clamped_values': True}
+    x_settings = {'image_size': 192, 'patch_size': 16, 'dim': 64, 'e_depth': 2, 'e_heads': 6, 'emb_dim':8, 'd_depth': 2, 'd_heads': 16, 'clamped_values': True}
     debug = False # Debugging your model on CPU is leagues easier
     if debug:
         device = torch.device('cpu')
@@ -348,10 +353,9 @@ def main():
     print('Total model parameters:\n\tTrainable: {}\n\tUntrainable: {}'.format(*(get_parameter_count(model))))
 
     # With AdamW
-    #optimizer = optim.Adam(model.parameters(), lr=1e-2)
+    optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay = 1e-4)
     #optimizer = optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
-    optimizer = AdaBelief(model.parameters(), lr=1e-2, weight_decay=1e-4, print_change_log=False)
-    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=125)
+    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=len(train_dataloader)*10, eta_min = 1e-6)
 
     epochs = 1000000
     max_seq_len = 227
@@ -381,7 +385,7 @@ def main():
                 loss.backward()
                 running_loss += loss.item()
 
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
             optimizer.step()
             scheduler.step()
             